8f752ddeec619f3b38bc440baf45597b39b0539b
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-03-21 16:59:13 -0300

    User delete

--- src/main/java/com/vitorpereira/courseProject/resources/UserResource.java
+++ src/main/java/com/vitorpereira/courseProject/resources/UserResource.java
@@ -35,4 +35,10 @@ public class UserResource {
         obj = userServices.insert(obj);
         return ResponseEntity.ok().body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userServices.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/vitorpereira/courseProject/services/UserServices.java
+++ src/main/java/com/vitorpereira/courseProject/services/UserServices.java
@@ -24,6 +24,11 @@ public class UserServices {
     }
 
     public User insert(User obj){
+
         return repository.save(obj);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
     }
 }
