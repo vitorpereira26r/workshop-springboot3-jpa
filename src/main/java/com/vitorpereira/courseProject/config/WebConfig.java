c7ab15be8105c7e1de334a3192c81223a6ea164f
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-10-19 15:15:43 -0300

    Allowing the frontend in production to access the API

--- src/main/java/com/vitorpereira/courseProject/config/WebConfig.java
+++ src/main/java/com/vitorpereira/courseProject/config/WebConfig.java
@@ -22,6 +22,7 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://vitor-pereira-workshop-spring-boot.com");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
